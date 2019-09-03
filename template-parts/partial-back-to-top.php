f5c85f02ad9e3dab640f63b478a1b3c941280889
Nic Ford <nicdford@gmail.com>
2019-09-03 15:28:28 -0700

    Setup initial config for adding a back to top button on the frontend

---
+++ template-parts/partial-back-to-top.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part for displaying back to top button
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WSU_WP_Lodge
+ */
+
+?>
+
+<div class="p-back-to-top_container">
+    <a href="#" class="p-back-to-top_link">
+        <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" x="0px" y="0px" viewBox="0 0 100 125"><g transform="translate(0,-952.36218)"><path style="color:#000000;enable-background:accumulate;" d="m 81.000015,1002.3622 -22,-38.00002 -40.00003,0 22,38.00002 -22,38 40.00003,0 z" fill="#000000" stroke="none" marker="none" visibility="visible" display="inline" overflow="visible"/></g></svg>
+    </a>
+</div>
