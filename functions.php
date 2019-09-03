d2df3dc994d0a96777642cf33c8557e79acadc38
Nic Ford <nicdford@gmail.com>
2019-09-03 15:29:11 -0700

    Enable theme default enqueues

--- functions.php
+++ functions.php
@@ -135,15 +135,15 @@ add_action( 'widgets_init', 'wsuwp_lodge_widgets_init' );
  * Enqueue scripts and styles.
  */
 function wsuwp_lodge_scripts() {
-	// wp_enqueue_style( 'wsuwp-lodge-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'wsuwp-lodge-style', get_stylesheet_uri() );
 
-	// wp_enqueue_script( 'wsuwp-lodge-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'wsuwp-lodge-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	// wp_enqueue_script( 'wsuwp-lodge-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'wsuwp-lodge-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-	// 	wp_enqueue_script( 'comment-reply' );
-	// }
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'wsuwp_lodge_scripts' );
 
