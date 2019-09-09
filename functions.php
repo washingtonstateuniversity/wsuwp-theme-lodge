956b9a8d612abde6b401d4ba1db0d5a23b6576de
Nic Ford <nicdford@gmail.com>
2019-09-09 11:40:20 -0700

    Cleanup classes and configure optional global base styles

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@
  */
 require_once 'classes/class-wsuwp-lodge.php';
 require_once 'classes/class-wsuwp-lodge-actions.php';
+require_once 'classes/class-wsuwp-lodge-customizer.php';
 require_once 'classes/class-wsuwp-lodge-shortcodes.php';
 
 /**
@@ -17,10 +18,7 @@ require_once 'classes/class-wsuwp-lodge-shortcodes.php';
  */
 add_action('after_setup_theme', 'WSU_WP_Lodge::theme_setup');
 add_action('widgets_init', 'WSU_WP_Lodge::add_sidebars_to_widgets_area');
-add_action('customize_register', 'WSU_WP_Lodge::add_social_to_customizer');
-add_action('customize_register', 'WSU_WP_Lodge::add_global_options_to_customizer');
 add_action('wp_enqueue_scripts', 'WSU_WP_Lodge::enqueue_scripts');
-add_action('back_to_top', 'WSU_WP_Lodge::display_back_to_top');
 
 /**
  * Filters
