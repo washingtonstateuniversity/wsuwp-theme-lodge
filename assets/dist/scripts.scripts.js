1793505e929a81737bdcb91be3b13852c6062028
Nic Ford <nicdford@gmail.com>
2019-09-09 11:43:42 -0700

    Cleanup frontend assets folders

--- assets/dist/scripts.scripts.js
+++ assets/dist/scripts.scripts.js
@@ -97,63 +97,8 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/main.scss */ "./assets/src/scss/main.scss");
 /* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _js_customizer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/customizer */ "./assets/src/js/customizer.js");
-/* harmony import */ var _js_customizer__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_customizer__WEBPACK_IMPORTED_MODULE_1__);
 
 
-
-
-/***/ }),
-
-/***/ "./assets/src/js/customizer.js":
-/*!*************************************!*\
-  !*** ./assets/src/js/customizer.js ***!
-  \*************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-/**
- * File customizer.js.
- *
- * Theme Customizer enhancements for a better user experience.
- *
- * Contains handlers to make Theme Customizer preview reload changes asynchronously.
- */
-
-( function( $ ) {
-
-	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-title a' ).text( to );
-		} );
-	} );
-	wp.customize( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-description' ).text( to );
-		} );
-	} );
-
-	// Header text color.
-	wp.customize( 'header_textcolor', function( value ) {
-		value.bind( function( to ) {
-			if ( 'blank' === to ) {
-				$( '.site-title, .site-description' ).css( {
-					'clip': 'rect(1px, 1px, 1px, 1px)',
-					'position': 'absolute'
-				} );
-			} else {
-				$( '.site-title, .site-description' ).css( {
-					'clip': 'auto',
-					'position': 'relative'
-				} );
-				$( '.site-title a, .site-description' ).css( {
-					'color': to
-				} );
-			}
-		} );
-	} );
-} )( jQuery );
 
 
 /***/ }),
@@ -170,4 +115,4 @@ __webpack_require__.r(__webpack_exports__);
 /***/ })
 
 /******/ });
-//# sourceMappingURL=bundle.scripts.js.map
+//# sourceMappingURL=scripts.scripts.js.map
