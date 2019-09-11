e5bebd199ca1894a8b89e8e453fbdcc35be99a92
Nic Ford <nicdford@gmail.com>
2019-09-11 11:15:53 -0700

    Abstract content entry headers into template file

---
+++ template-parts/content/content-entry-header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying entry headers on posts & pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WSU_WP_Lodge
+ */
+
+ ?>
+
+<header class="entry-header">
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+</header><!-- .entry-header -->
--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -10,9 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+
+	<?php
+	/*
+	* Include Content Entry Header
+	*/
+	get_template_part( 'template-parts/content/content', 'entry-header' );
+	?>
 
 	<?php //wsuwp_lodge_post_thumbnail(); ?>
 
