920d34b7600c5a142808b5e21cad8c5484935410
Nic Ford <nicdford@gmail.com>
2019-09-11 11:39:32 -0700

    Abstract header into template part

--- header.php
+++ header.php
@@ -24,35 +24,11 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wsuwp-lodge' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$wsuwp_lodge_description = get_bloginfo( 'description', 'display' );
-			if ( $wsuwp_lodge_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $wsuwp_lodge_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wsuwp-lodge' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'primary-nav',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<?php
+	/*
+	* Include global footer template
+	*/
+	get_template_part( 'template-parts/global/global', 'header' );
+	?>
 
 	<div id="content" class="site-content <?php do_action( 'WSU_WP_Lodge_content_width' ); ?>">
