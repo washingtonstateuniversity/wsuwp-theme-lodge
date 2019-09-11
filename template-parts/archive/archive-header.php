35b9d63d90e6450de1a6b92923390f63a34b049b
Nic Ford <nicdford@gmail.com>
2019-09-11 10:27:23 -0700

    Update archive template/parts

---
+++ template-parts/archive/archive-header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying archive page headers
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WSU_WP_Lodge
+ */
+
+ ?>
+
+<header class="page-header">
+	<?php
+	the_archive_title( '<h1 class="page-title">', '</h1>' );
+	the_archive_description( '<div class="archive-description">', '</div>' );
+	?>
+</header><!-- .page-header -->
