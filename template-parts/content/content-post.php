0fe9f7741f61dc6d6844d9a28507a126d9db0ef2
Nic Ford <nicdford@gmail.com>
2019-09-11 11:17:34 -0700

    Utilize post entry content header template

--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -10,26 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				// wsuwp_lodge_posted_on();
-				// wsuwp_lodge_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php // wsuwp_lodge_post_thumbnail(); ?>
+	<?php
+	/*
+	* Include Content Entry Header
+	*/
+	get_template_part( 'template-parts/content/content', 'entry-header' );
+	?>
 
 	<div class="entry-content">
 		<?php
