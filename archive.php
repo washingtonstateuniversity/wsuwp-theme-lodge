419cd431ccd6fdce400f3e5fe9d5c15f4120af07
Nic Ford <nicdford@gmail.com>
2019-09-27 16:59:46 -0700

    Update archive template to point to archive parts

--- archive.php
+++ archive.php
@@ -22,21 +22,32 @@ get_header();
 			get_template_part( 'template-parts/archive/archive', 'header' );
 			?>
 
+			<section class="archive-content-wrapper">
+
+				<div class="archive-content-list">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/archive/archive', get_post_type() );
+
+					endwhile;
+					?>
+				</div>
+
+				<?php
+				get_sidebar();
+				?>
+
+			</section>
+
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/archive/archive', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
 
 		else :
 
@@ -46,8 +57,8 @@ get_header();
 		?>
 
 		</main><!-- #main -->
+
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
